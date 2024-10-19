7eac47ec5f928082d26ba6ac2f73914d86325f63
pedroafk <pedro.pessoal1996@gmail.com>
2024-10-19 11:01:59 -0300

    Laravel e Vuejs conectados

---
+++ resources/js/components/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <p>{{ message }}</p>
+      <button @click="changeMessage">Click me</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        message: "Hello World from Vue.js!"
+      };
+    },
+    methods: {
+      changeMessage() {
+        this.message = "You clicked the button!";
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* Adicione seus estilos aqui */
+  </style>
+  
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ReservationController;
+use Illuminate\Support\Facades\Route;
 
 // Rota para obter o CSRF token
 Route::get('api/csrf-token', [ReservationController::class, 'getCsrfToken']);
@@ -11,3 +12,8 @@ Route::get('api/reservations/{id}', [ReservationController::class, 'show']);
 Route::post('api/reservations', [ReservationController::class, 'store']);
 Route::put('api/reservations/{id}', [ReservationController::class, 'update']);
 Route::delete('api/reservations/{id}', [ReservationController::class, 'destroy']);
+
+// Rota base
+Route::get('/', function () {
+    return view('welcome');
+});
